4f60f108b5b0444007bb0ff4116e88023853ef92
Panagiotis Katsampiris <lostinmyworld222@gmail.com>
2025-12-11 22:41:27 +0200

    - remove unneeded try-catch

--- InstaWatchdogApp/Program.cs
+++ InstaWatchdogApp/Program.cs
@@ -18,40 +18,32 @@ var instagramConsumer = serviceProvider.GetRequiredService<IInstagramConsumer>()
 var discordSharer = serviceProvider.GetRequiredService<IDiscordSharer>();
 var gistConsumer = serviceProvider.GetRequiredService<IGistConsumer>();
 
-try
+var state = await gistConsumer.LoadPreviousState(environmentVars.GistOptions);
+Console.WriteLine($"LastInstagramId from state: {state.LastPostId ?? "null"}.");
+
+var instaRequest = new InstagramRequest(environmentVars.InstagramToken, "insta-watchdog/1.0", HowManyPostsToFetch: 20);
+// Retrieve the latest Instagram posts
+var instagramPosts = await instagramConsumer.RetrievePostsAsync(instaRequest);
+if (instagramPosts.Count == 0)
 {
-    var state = await gistConsumer.LoadPreviousState(environmentVars.GistOptions);
-    Console.WriteLine($"LastInstagramId from state: {state.LastPostId ?? "null"}.");
-
-    var instaRequest = new InstagramRequest(environmentVars.InstagramToken, "insta-watchdog/1.0", HowManyPostsToFetch: 20);
-    // Retrieve the latest Instagram posts
-    var instagramPosts = await instagramConsumer.RetrievePostsAsync(instaRequest);
-    if (instagramPosts.Count == 0)
-    {
-        await Console.Error.WriteLineAsync("No posts returned from Instagram.");
-        return;
-    }
-
-    // Find 1 not shared in Discord (its post id will be located in state if it is)
-    var postToShare = parser.GetOlderNotSharedPost(instagramPosts, state);
-    if (postToShare is null)
-    {
-        Console.WriteLine("No new post to share.");
-        return;
-    }
-
-    // Share Instagram link to Discord
-    var discordRequest = parser.BuildDiscordRequest(postToShare);
-    await discordSharer.SendToDiscord(discordRequest, environmentVars.DiscordWebHook);
-
-    // Saved shared post Id to Gist
-    state.LastPostId = postToShare.Id;
-    await gistConsumer.SaveCurrentState(state, environmentVars.GistOptions);
-
-    Console.WriteLine("Finished.");
+    await Console.Error.WriteLineAsync("No posts returned from Instagram.");
+    return;
 }
-catch (Exception ex)
+
+// Find 1 not shared in Discord (its post id will be located in state if it is)
+var postToShare = parser.GetOlderNotSharedPost(instagramPosts, state);
+if (postToShare is null)
 {
-    await Console.Error.WriteLineAsync("Error in InstaWatchdog:");
-    Console.Error.WriteLine(ex);
+    Console.WriteLine("No new post to share.");
+    return;
 }
+
+// Share Instagram link to Discord
+var discordRequest = parser.BuildDiscordRequest(postToShare);
+await discordSharer.SendToDiscord(discordRequest, environmentVars.DiscordWebHook);
+
+// Saved shared post Id to Gist
+state.LastPostId = postToShare.Id;
+await gistConsumer.SaveCurrentState(state, environmentVars.GistOptions);
+
+Console.WriteLine("Finished.");
